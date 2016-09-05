d87e8c9ac0600b074a8eb0a0e84cf8d979815432
bashhack <info@marclaughton.com>
2016-09-04 19:43:07 -0500

    Pushing work in progress

--- Organizing-A-Clojure-Project/project-organization-and-structure.clj
+++ Organizing-A-Clojure-Project/project-organization-and-structure.clj
@@ -49,3 +49,68 @@
 'inc
 'str
 ; etc...
+
+;;;; ---------------------------------------------------------------------------
+;;;; Working with the `def` Keyword
+;;;; ---------------------------------------------------------------------------
+
+;;; Our primary method of storing the obejcts we reference is via `def` - even
+;;; other utilities like `defn` are still using `def` at their core.
+;;; Let's use `def` again for practice:
+
+(def metal-genres ["grindcore" "stoner rock" "gothic symphonic metal"])
+; => #'user/metal-genres
+
+metal-genres
+; => ["grindcore" "stoner rock" "gothic symphonic metal"]
+
+;;; (1) Here, we update the current namespace's map with the association between
+;;; the symbol we have created with the name `metal-genres` and the var,
+;;; the vector `["grindcore" "stoner rock" "gothic symphonic metal"]`
+
+;;; (2) Clojure finds a place to store the var
+
+;;; (3) The vector is stored
+
+;;; (4) An address to the symbol is written on the var
+
+;;; (5) We return the var `#'user/metal-genres`
+
+;;; The process above is called `interning` a var. We can view the entire map
+;;; of symbols-to-interned-vars using the method `ns-interns`:
+
+(ns-interns *ns*)
+; => {metal-genres #'user/metal-genres}
+
+(get (ns-interns *ns*) 'metal-genres)
+; => #'user/metal-genres
+
+;;; Using `ns-map` will return the map that the current namespace uses when
+;;; returning a var from a symbol name...the output, as the author stated, was
+;;; large...having this method available immediately reminded me of how I might
+;;; do the same in a Python script using the `dir` method: `dir(__name__)`
+
+;;; To get the var associated with the symbol from our map, we can use `deref`:
+
+(deref #'user/metal-genres)
+; => ["grindcore" "stoner rocker" "gothic symphonic metal"]
+
+;;; It's important to remember is that (deref #'user/metal-genres) is equal to:
+
+metal-genres
+; => ["grindcore" "stoner rock" "gothic symphonic metal"]
+
+;;; Let's get crazy for a bit!
+
+(def metal-genres ["deathcore" "folk metal" "goblin metal"])
+; => #'user/metal-genres
+
+metal-genres
+; => ["deathcore" "folk metal" "goblin metal"]
+
+;;; Wait - hold up...we just overwrote the first vector - we've just created
+;;; a name collision conflict!
+
+;;;; ---------------------------------------------------------------------------
+;;;; Creating and Switching Betwen Namespaces
+;;;; ---------------------------------------------------------------------------
